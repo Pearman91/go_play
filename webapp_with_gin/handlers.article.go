d215a53bc168ced3e79aee9a74fe044e53673c82
Jan Hruška <hru3kus@gmail.com>
2019-12-16 23:55:19 +0100

    add article handler

---
+++ webapp_with_gin/handlers.article.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+	 "github.com/gin-gonic/gin"
+)
+
+func showIndexPage(c *gin.Context) {
+	articles := getAllArticles
+
+	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home Page", "payload": articles,})
+}
--- webapp_with_gin/routes.go
+++ webapp_with_gin/routes.go
@@ -1,9 +1,9 @@
 package main
+
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
-
 
 func initializeRoutes() {
 	router.GET("/", func(c *gin.Context) {
