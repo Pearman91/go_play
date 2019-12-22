bbce121c9030d17db64741800e0dcdaf5133ceeb
Jan Hruška <hru3kus@gmail.com>
2019-12-22 23:54:32 +0100

    test band POST & GET

--- webapp_with_go_and_mux/src/webapp_go_mux/main_test.go
+++ webapp_with_go_and_mux/src/webapp_go_mux/main_test.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+type BandEntry struct {
+	bandname string `json:"bandname"`
+	genre string `json:"genre"`
+}
 
 func TestHandler(t *testing.T) {
 	// request bez body
@@ -61,7 +68,7 @@ func TestRouter(t *testing.T) {
 		t.Fatal(err)
 	}
 	
-	// test ze body je prazdne
+	// test ze body je spravne
 	respString := string(b)
 	expected := "Nazdar cype!!"
 	if respString != expected {
@@ -110,5 +117,44 @@ func TestStaticFileServer(t *testing.T) {
 
 	if expectedContentType != contentType{
 		t.Errorf("Content type mel byt: %s ale je: %s", expectedContentType, contentType)
+	}
+}
+
+
+func TestGetPostBandRouter(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	jsonValue := map[string]string{"bandname":"Nirvana","genre":"Grunge"}
+	jsonSend, _ := json.Marshal(jsonValue)
+	respPost, errPost := http.Post(mockServer.URL + "/band", "application/json", bytes.NewBuffer(jsonSend))
+
+	if errPost != nil { t.Fatal(errPost) }
+	if respPost.StatusCode != http.StatusOK {
+		t.Errorf("Status na picu: %d misto aby byl vcajku", respPost.StatusCode)
+	}
+
+	defer respPost.Body.Close()
+
+	respGet, errGet := http.Get(mockServer.URL + "/band")
+
+	if errGet != nil { t.Fatal(errGet) }
+	if respGet.StatusCode != http.StatusOK {
+		t.Errorf("Status na picu: %d misto aby byl vcajku", respGet.StatusCode)
+	}
+
+	defer respGet.Body.Close()
+
+	b, _ := ioutil.ReadAll(respGet.Body)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	
+	var respBand BandEntry
+	json.NewDecoder(respGet.Body).Decode(&respBand)
+	t.Log(b)
+	expected := BandEntry{bandname:"Nirvana", genre: "Grunge"}
+	if respBand != expected {
+		t.Errorf("Response mela byt: %s ale je: %s", expected, respBand)
 	}
 }
